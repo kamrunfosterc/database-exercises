a42ccc6320e89e155790bab461f403b22011d53a
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-24 09:52:06 -0500

    updated walkthrough on delete exercise

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,5 +1,9 @@
 use codeup_testing_db;# shows what data base your calling to work with
 
+select * from albums where release_date > 1991;
+select * from albums where genre = 'disco';
+select * from albums where artist = 'Whitney Houston';
+
 #select statements for
 #albums relased after 91
 delete from albums where genre = 'disco';
@@ -7,3 +11,15 @@ delete from albums where genre = 'disco';
 
 delete from albums where artist = 'Whitney Houston'; #for delete portion
 # albums by whitney houston
+
+#todo Sam walkthrough
+#
+# select * from albums;
+#
+# delete from albums where release_date > 1991;
+# delete from albums where genre = 'disco';
+# delete from albums where artist = 'Nirvana';
+#
+# select * from albums where release_date > 1991;
+# select * from albums where genre = 'disco';
+# select * from albums where artist = 'Whitney Houston';
--- update_exercises.sql
+++ update_exercises.sql
@@ -22,18 +22,18 @@ set sales = sales *10;
 
 
 #todo Sam walk through
-
-select * from albums;
-
-update albums set sales = sales * 10;
-update albums set release_date = 1800 where artist = 'Michael Jackson';#error out of range, bc YEAR goes only to 1901
-
-update albums
-set artist = 'Peter Jackson'
-where artist = 'Michael Jackson';
-
-select * from albums where release_date < 1980;
-select * from albums where artist = 'Michael Jackson';
+#
+# select * from albums;
+#
+# update albums set sales = sales * 10;
+# update albums set release_date = 1800 where artist = 'Michael Jackson';#error out of range, bc YEAR goes only to 1901
+#
+# update albums
+# set artist = 'Peter Jackson'
+# where artist = 'Michael Jackson';
+#
+# select * from albums where release_date < 1980;
+# select * from albums where artist = 'Michael Jackson';
 
 
 
