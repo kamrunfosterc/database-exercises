c9e591827c90e31b46f4b3bf0bb795446cd6327d
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-25 09:57:45 -0500

    completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -4,7 +4,8 @@ use employees;
 #todo part 2
 SELECT *
 from employees.employees e
-where CONCAT(first_name, last_name) like 'e%e';
+where Concat(first_name like 'e%e' and last_name like 'e%e');
+# where CONCAT(first_name, last_name) like 'e%e'; # alt: Concat(first_name like 'e%e' and last_name like 'e%e')
 # above returns firstnames starting w e and lastnames ending with e
 
 
@@ -18,16 +19,27 @@ select * from employees.employees e where month(birth_date) = 12 and day(birth_d
 # 4Find all employees hired in the 90s and born on Christmas — 362 rows.
 select *
 from employees.employees e
-where year(hire_date) between 1990 and 1999;
+where year(hire_date) between 1990 and 1999 and month(birth_date) = 12 and day(birth_date) = 25;
 #got it to show all years in the 90's however not in any particular order
 
-
+#todo part 5
 # 5Change the query for employees hired in the 90s and born on Christmas such that the first
 # result is the
 # oldest employee who was hired last. It should be Khun Bernini.
+select *
+from employees.employees e
+where year(hire_date) between 1990 and 1999 and month(birth_date) = 12 and day(birth_date) = 25 order by hire_date desc;
 
 # 6For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days
 # they have been working at the company (Hint: You might also need to use now() or curdate()).
+#todo 6
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days
+# they have been working at the company (Hint: You might also need to use now() or curdate()).
+select first_name, last_name, datediff(now(), hire_date)
+from employees.employees e
+where year(hire_date) between 1990 and 1999
+  and month(birth_date) = 12 and day(birth_date) = 25
+#CIERRA HELP
 
 
 
